656e3ec42104e82abe10661b5d147cc9e87601f6
HaLin Kim <75435113+kimhalin@users.noreply.github.com>
2023-04-10 21:09:44 +0900

    Time: 130 ms (72.31%), Space: 15.1 MB (49.13%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -0,0 +1,13 @@
+class Solution:
+    # 가장 마지막 인덱스로 가기 위한 최소 점프 횟수
+    def jump(self, nums):
+        n, start, end, step = len(nums), 0, 0, 0
+        while end < n - 1:
+            step += 1
+            maxend = end + 1
+            for i in range(start, end + 1):
+                if i + nums[i] >= n - 1:
+                    return step
+                maxend = max(maxend, i + nums[i])
+            start, end = end + 1, maxend
+        return step

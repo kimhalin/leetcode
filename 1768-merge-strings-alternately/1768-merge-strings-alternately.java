7a32855e4dcb0f32ba226a4bfc3db40dad567b52
HaLin Kim <75435113+kimhalin@users.noreply.github.com>
2023-05-08 15:06:33 +0900

    Time: 1 ms (71.74%), Space: 41 MB (43.21%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -1,29 +1,13 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        String[] words1 = word1.split("");
-        String[] words2 = word2.split("");
-        String answer = "";
-        if (words1.length > words2.length) {
-            int j = 0;
-            for (int i = 0; i < words2.length; i++) {
-                answer = answer + words1[j++];
-                answer = answer + words2[i];
-            }
-
-            while(j < words1.length) {
-                answer = answer + words1[j++];
-            }
+        int n = word1.length(), m = word2.length(), i = 0, j = 0;
+        StringBuilder res = new StringBuilder();
+        while(i < n || j < m) {
+            if (i < word1.length())
+                res.append(word1.charAt(i++));
+            if (j < word2.length())
+                res.append(word2.charAt(j++));
         }
-        else {
-            int j =0;
-            for (int i = 0; i < words1.length; i++) {
-                answer = answer + words1[i];
-                answer = answer + words2[j++];
-            }
-            while (j < words2.length) {
-                answer = answer + words2[j++];
-            }
-        }
-        return answer;
+        return res.toString();
     }
 }

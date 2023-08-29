633ddd25760dc48b331b54071fbf8c4d7fcebeae
HaLin Kim <75435113+kimhalin@users.noreply.github.com>
2023-08-30 02:18:35 +0900

    Time: 7 ms (59.12%), Space: 43 MB (88.83%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,29 +1,29 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-        Stack<Integer> stack = new Stack<>();
-        // 숫자 스택 사용
-        for (String token: tokens) {
-            if (token.matches("[-+]?\\d*\\.?\\d+")) {
-                stack.push(Integer.parseInt(token));
-                continue;
-            }
-            int first = stack.pop();
-            int second = stack.pop();
-            switch(token) {
-                case "+":
-                    stack.push(first+second);
-                    break;
-                case "-":
-                    stack.push(second-first);
-                    break;
-                case "*":
-                    stack.push(first*second);
-                    break;
-                case "/":
-                    stack.push(second/first);
-                    break;                                                        
+        Stack<Integer> st2 = new Stack<>();
+
+        for(String s : tokens){
+            if(s.equals("+") || s.equals("*") || s.equals("/") || s.equals("-")){
+                int b = st2.pop();
+                int a = st2.pop();
+                int res = cal(a, b, s.charAt(0));
+                st2.push(res);
+                
+            } else {
+                st2.push(Integer.parseInt(s));
             }
         }
-        return stack.peek();
+        return st2.pop();
+    }
+    private int cal(int a, int b, char oprtr){
+        if(oprtr == '+'){
+            return a + b;
+        } else if(oprtr == '-'){
+            return a - b;
+        } else if(oprtr == '*'){
+            return a * b;
+        } else {
+            return a / b;
+        }
     }
 }

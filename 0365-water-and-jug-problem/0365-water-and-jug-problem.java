e86b3f900e3003b50fad8d10dbb93aff722697f1
HaLin Kim <75435113+kimhalin@users.noreply.github.com>
2024-01-04 23:43:25 +0900

    Time: 0 ms (100%), Space: 40.2 MB (61.8%) - LeetHub

--- 0365-water-and-jug-problem/0365-water-and-jug-problem.java
+++ 0365-water-and-jug-problem/0365-water-and-jug-problem.java
@@ -1,32 +1,21 @@
 class Solution {
     public boolean canMeasureWater(int jug1Capacity, int jug2Capacity, int targetCapacity) {
-        Deque<int[]> queue = new ArrayDeque<>();
-        Set<String> seen = new HashSet<>();
-
-        queue.addLast(new int[]{0, 0});
-        seen.add(0 + "," + 0);
-        while (queue.size() > 0) {
-            int[] currSearch = queue.removeFirst();
-            int a = currSearch[0], b = currSearch[1];
-
-            if (a + b == targetCapacity) return true;
-
-            List<int[]> nextMoves = new ArrayList<>();
-            nextMoves.add(new int[]{jug1Capacity, b});
-            nextMoves.add(new int[]{a, jug2Capacity});
-            nextMoves.add(new int[]{0, b});
-            nextMoves.add(new int[]{a, 0});
-            nextMoves.add(new int[]{Math.max(0, a - (jug2Capacity - b)), Math.min(a + b, jug2Capacity)});
-            nextMoves.add(new int[]{Math.min(a + b, jug1Capacity), Math.max(0, b - (jug1Capacity - a))});
-
-            for (int[] move : nextMoves) {
-                String key = move[0] + "," + move[1];
-                if (!seen.contains(key)) {
-                    seen.add(key);
-                    queue.addLast(move);
-                }
-            }
+        if(jug1Capacity+jug2Capacity<targetCapacity)
+        {
+            return false;
         }
-        return false;
+        if(jug1Capacity==targetCapacity||jug2Capacity==targetCapacity||jug1Capacity+jug2Capacity==targetCapacity)
+        {
+            return true;
+        }
+        return targetCapacity%gcd(jug1Capacity,jug2Capacity)==0;
+    }
+    static int gcd(int a,int b)
+    {
+        if(b==0)
+        {
+            return a;
+        }
+        return gcd(b,a%b);
     }
 }

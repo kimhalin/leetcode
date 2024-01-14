86868d2bfed1020124649fe94ef947e2231a8b50
HaLin Kim <75435113+kimhalin@users.noreply.github.com>
2024-01-15 01:52:20 +0900

    Time: 0 ms (100%), Space: 40.1 MB (34.56%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,10 +1,15 @@
 class Solution {
     public int climbStairs(int n) {
-        int a = 1, b = 1;
-        while (n-- > 0) {
-            b += a;
-            a = b - a;
+        if (n == 0 || n == 1)
+            return 1;
+        
+        int[] dp = new int[n+1];
+        dp[0] = dp[1] = 1;
+
+        for (int i = 2; i <=n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
-        return a;
+
+        return dp[n];
     } 
 }

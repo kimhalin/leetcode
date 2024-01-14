605527628cc56cff7c815a95f2d578e36a00295a
HaLin Kim <75435113+kimhalin@users.noreply.github.com>
2024-01-15 01:46:54 +0900

    Time: 0 ms (100%), Space: 40.5 MB (10.81%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int climbStairs(int n) {
+        int a = 1, b = 1;
+        while (n-- > 0) {
+            b += a;
+            a = b - a;
+        }
+        return a;
+    } 
+}

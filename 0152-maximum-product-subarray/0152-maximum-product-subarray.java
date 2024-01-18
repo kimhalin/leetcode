c70aa61371722da5b89410c2c4e28610e4980622
HaLin Kim <75435113+kimhalin@users.noreply.github.com>
2024-01-18 21:24:59 +0900

    Time: 0 ms (100%), Space: 44.9 MB (25.44%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,18 +1,20 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        int max = nums[0], min = nums[0], result = nums[0];
-        // negative multiple negative might be max
-        for(int i=1; i<nums.length; i++){
-            int temp =max;
-            max = Math.max(Math.max(temp*nums[i], nums[i]*min), nums[i]);
-            // overflow only happened when negative multiple
-            // result cannot be overflow
-            // then if negative multiple others already overflow
-            // we can ignore the result after the overflow
-            if(nums[i]>0 && Integer.MIN_VALUE/nums[i]>min) min = nums[i];
-            else min = Math.min(Math.min(temp*nums[i], nums[i]*min), nums[i]);
-            result = Math.max(result, max);
-        }
-        return result;
+       int ans = Integer.MIN_VALUE;
+       int prod=1;
+       for(int i=0;i<nums.length;i++){
+           prod*=nums[i];
+           ans=Math.max(ans,prod);
+           if(prod==0)
+                prod=1;
+       }
+       prod=1;
+       for(int i=nums.length-1;i>=0;i--){
+           prod*=nums[i];
+           ans=Math.max(ans,prod);
+           if(prod==0)
+                prod=1;
+       }
+       return ans; 
     }
 }

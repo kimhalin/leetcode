32e5e7026c9b1c4f3326c496097b9126fda2871e
HaLin Kim <75435113+kimhalin@users.noreply.github.com>
2024-01-20 17:33:15 +0900

    Time: 2 ms (99.13%), Space: 45.2 MB (41.78%) - LeetHub

---
+++ 0942-di-string-match/0942-di-string-match.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] diStringMatch(String s) {
+        int n = s.length(), left = 0, right = n;
+        int[] res = new int[n + 1];
+        for (int i =0; i < n; i++)
+            res[i] = s.charAt(i) == 'I' ? left++ : right--;
+        res[n] = left;
+        return res;
+    }
+}

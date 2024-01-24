4a877361d85d1bae68d067cd49cf4d88d93a7b98
HaLin Kim <75435113+kimhalin@users.noreply.github.com>
2024-01-24 13:49:45 +0900

    Time: 3 ms (61.34%), Space: 44.5 MB (63.04%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
@@ -15,14 +15,25 @@
  */
 class Solution {
     public boolean findTarget(TreeNode root, int k) {
-        HashSet<Integer> set = new HashSet<>();
-        return dfs(root, set, k);
-    }
+        Set<Integer> set = new HashSet<>();
+        Queue<TreeNode> q = new LinkedList<>();
 
-    public boolean dfs(TreeNode root, HashSet<Integer> set, int k) {
-        if (root == null) return false;
-        if (set.contains(k - root.val)) return true;
-        set.add(root.val);
-        return dfs(root.left, set, k) || dfs(root.right, set, k);
+        q.offer(root);
+
+        while(!q.isEmpty()) {
+            TreeNode node = q.poll();
+
+
+            if (set.contains(k - node.val))
+                return true;
+
+           set.add(node.val);
+            if (node.right != null)
+                q.offer(node.right);
+            if (node.left != null)
+                q.offer(node.left);
+        }
+
+        return false;
     }
 }

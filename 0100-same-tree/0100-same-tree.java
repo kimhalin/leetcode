7bd8051ff86e2fa6daf4db77851632eb301378bc
HaLin Kim <75435113+kimhalin@users.noreply.github.com>
2024-01-24 14:18:00 +0900

    Time: 0 ms (100%), Space: 41.3 MB (9.29%) - LeetHub

---
+++ 0100-same-tree/0100-same-tree.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null || p.val != q.val) {
+            return false;
+        }
+        // Recursively check if the left and right subtrees are identical
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}

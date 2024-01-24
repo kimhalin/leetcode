9aa2f2c9853a762b1d42fa4ee744cfc8055d6d33
HaLin Kim <75435113+kimhalin@users.noreply.github.com>
2024-01-24 23:24:26 +0900

    Time: 8 ms (99.09%), Space: 109 MB (94.94%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,46 +1,43 @@
-class JointSet {
-    int n;
-    int[] parent;
+class DisjointSetUnion {
+    private int[] parent;
+    private int N;
 
-    public JointSet(int n) {
-        this.n = n;
-        this.parent = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            parent[i] = i;
+    public DisjointSetUnion(int n) {
+        this.N = n;
+        this.parent = new int[this.N];
+        for (int i = 0; i < this.N; i++) {
+            this.parent[i] = i;
         }
     }
 
-    public boolean isConnected(int u, int v) {
+    public boolean areConnected(int u, int v) {
         return find(u) == find(v);
     }
 
     public void union(int u, int v) {
-        int a = find(u);
-        int b = find(v);
-
-        this.parent[a] = b;
+        if (u != v) {
+            int a = find(u);
+            int b = find(v);
+            parent[a] = b;
+        }
     }
 
-    public int find(int u) {
+    private int find(int u) {
         int x = u;
-        while (x != this.parent[x]) {
+        while(x != this.parent[x])
             x = this.parent[x];
-        }
-
+        
         this.parent[u] = x;
         return x;
     }
 }
-
 class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-        JointSet set = new JointSet(n);
-
+        DisjointSetUnion set = new DisjointSetUnion(n);
         for (int[] edge : edges) {
             set.union(edge[0], edge[1]);
         }
 
-        return set.isConnected(source, destination);
+        return set.areConnected(source, destination);
     }
 }

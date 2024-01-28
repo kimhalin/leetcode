7f92438733914f9cf921f43d019c8ced26e54fb7
HaLin Kim <75435113+kimhalin@users.noreply.github.com>
2024-01-29 03:04:57 +0900

    Time: 0 ms (100%), Space: 43.8 MB (47.76%) - LeetHub

--- 0559-maximum-depth-of-n-ary-tree/0559-maximum-depth-of-n-ary-tree.java
+++ 0559-maximum-depth-of-n-ary-tree/0559-maximum-depth-of-n-ary-tree.java
@@ -19,24 +19,10 @@ class Node {
 
 class Solution {
     public int maxDepth(Node root) {
-        if (root == null) return 0;
-        int answer = 0;
-
-        Queue<Node> q = new LinkedList<>();
-        q.offer(root);
-
-        while(!q.isEmpty()) {
-            int n = q.size();
-
-            for (int i = 0; i < n; i++) {
-                Node node = q.poll();
-
-                for (Node tmp : node.children) {
-                    q.offer(tmp);
-                }
-            }
-            answer++;
-        }
-        return answer;
+        if(root==null) return 0;
+        int max=0;
+        for(Node child: root.children) 
+            max=Math.max(max,maxDepth(child));
+        return max+1;
     }
 }

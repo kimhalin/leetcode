e18013c0cb54bf71e12fd9652ceb7871228ad6e0
HaLin Kim <75435113+kimhalin@users.noreply.github.com>
2024-01-31 23:37:40 +0900

    Time: 0 ms (100%), Space: 41.4 MB (36.42%) - LeetHub

--- 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.java
+++ 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.java
@@ -14,35 +14,26 @@
  * }
  */
 class Solution {
-    public boolean isCousins(TreeNode root, int A, int B) {
-        if (root == null) return false;
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            boolean isAexist = false;		
-            boolean isBexist = false;		
-            for (int i = 0; i < size; i++) {
-                TreeNode cur = queue.poll();
-                if (cur.val == A) isAexist = true;
-                if (cur.val == B) isBexist = true;
-                if (cur.left != null && cur.right != null) { 
-                    if (cur.left.val == A && cur.right.val == B) { 
-                        return false;
-                    }
-                    if (cur.left.val == B && cur.right.val == A) { 
-                        return false;
-                    }
-                }
-                if (cur.left != null) {
-                    queue.offer(cur.left);
-                }
-                if (cur.right != null) {
-                    queue.offer(cur.right);
-                }
-            }
-            if (isAexist && isBexist)  return true;
+    TreeNode xParent = null;
+    TreeNode yParent = null;
+    int xDepth = -1, yDepth = -1;
+
+    public boolean isCousins(TreeNode root, int x, int y) {
+        getDepthAndParent(root, x, y, 0, null);
+        return xDepth == yDepth && xParent != yParent? true: false;
+    }
+
+    public void getDepthAndParent(TreeNode root, int x, int y, int depth, TreeNode parent) {
+        if (root == null) return;
+        if (root.val == x) {
+            xParent = parent;
+            xDepth = depth;
+        } else if (root.val == y) {
+            yParent = parent;
+            yDepth = depth;
         }
-        return false;
+
+        getDepthAndParent(root.left, x, y, depth + 1, root);
+        getDepthAndParent(root.right, x, y, depth + 1, root);
     }
 }

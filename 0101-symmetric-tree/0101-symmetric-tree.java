189faa008b183a73967abac22db978aaa4a887ae
HaLin Kim <75435113+kimhalin@users.noreply.github.com>
2024-01-31 23:25:32 +0900

    Time: 0 ms (100%), Space: 41.7 MB (33.96%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -15,14 +15,13 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        return root == null || isSymmetricHelp(root.left, root.right);
+        return symmetric(root.left, root.right);
     }
 
-    private boolean isSymmetricHelp(TreeNode left, TreeNode right) {
-        if (left == null || right == null)
-            return left==right;
-        if (left.val != right.val)
-            return false;
-        return isSymmetricHelp(left.left, right.right) && isSymmetricHelp(left.right, right.left);
+    public boolean symmetric(TreeNode left, TreeNode right) {
+        if (left == null && right == null) return true;
+        if (left == null || right == null || left.val != right.val) return false;
+        
+        return symmetric(left.left, right.right) && symmetric(left.right, right.left);
     }
 }

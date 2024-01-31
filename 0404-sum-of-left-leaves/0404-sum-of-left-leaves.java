0ffe36ea22db18a276330e41c12c1e9f7f18dd40
HaLin Kim <75435113+kimhalin@users.noreply.github.com>
2024-01-31 23:12:47 +0900

    Time: 0 ms (100%), Space: 41 MB (68.18%) - LeetHub

--- 0404-sum-of-left-leaves/0404-sum-of-left-leaves.java
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.java
@@ -13,19 +13,19 @@
  *     }
  * }
  */
+
 class Solution {
-    int res = 0;
     public int sumOfLeftLeaves(TreeNode root) {
-        // 왼쪽 서브트리에 있는 왼쪽 노드 모두 구하기
-        // 오른쪽 서브 트리에 있는 왼쪽 노드 모두 구하기
+        if (root == null) return 0;
+        int ans = 0;
+        
         if (root.left != null) {
-            if (root.left.left == null && root.left.right == null)
-                res += root.left.val;
-            else
-                sumOfLeftLeaves(root.left);
+            if (root.left.left == null && root.left.right == null) ans += root.left.val;
+            else ans += sumOfLeftLeaves(root.left);
         }
 
-        if (root.right != null) sumOfLeftLeaves(root.right);
-        return res;
+        ans += sumOfLeftLeaves(root.right);
+
+        return ans;
     }
 }

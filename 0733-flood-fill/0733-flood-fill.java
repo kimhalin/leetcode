e6977df03acfc3e62922426ca86872e9a8b3a2d8
HaLin Kim <75435113+kimhalin@users.noreply.github.com>
2024-01-31 13:01:47 +0900

    Time: 0 ms (100%), Space: 44.6 MB (62.87%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,30 +1,17 @@
 class Solution {
-    boolean[][] visited;
-    int[] dx = new int[] {0, 0, -1, 1};
-    int[] dy = new int[] {-1, 1, 0, 0};
-
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        visited = new boolean[image.length][image[0].length];
-        
+        if (image[sr][sc] == color) return image;
         flood(image, sr, sc, image[sr][sc], color);
-
         return image;
     }
 
-    public void flood(int[][]image, int sr, int sc, int color, int newColor) {
-        if (sr < 0 || sc < 0 || sr >= image.length || sc >= image[0].length)
-            return;
-        if (visited[sr][sc] == true)
-            return;
-        if (image[sr][sc] != color) {
-            visited[sr][sc] = true;
-            return;
-        }
-
+    public void flood(int[][] image, int sr, int sc, int color, int newColor) {
+        if (sr < 0 || sc < 0 || sr >= image.length || sc >= image[0].length || image[sr][sc] != color) return;
         image[sr][sc] = newColor;
-        visited[sr][sc] = true;
-        for (int i = 0; i < 4; i++) {
-            flood(image, sr + dx[i], sc + dy[i], color, newColor);
-        }
+
+        flood(image, sr - 1, sc, color, newColor);
+        flood(image, sr, sc - 1, color, newColor);
+        flood(image, sr + 1, sc, color, newColor);
+        flood(image, sr, sc + 1, color, newColor);
     }
 }

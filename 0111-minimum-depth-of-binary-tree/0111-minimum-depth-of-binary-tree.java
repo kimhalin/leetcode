df764da08358fd29b3e9859e3e65fcb47618cae9
HaLin Kim <75435113+kimhalin@users.noreply.github.com>
2024-02-01 17:29:21 +0900

    Time: 4 ms (74.69%), Space: 63.4 MB (30.86%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
@@ -15,27 +15,20 @@
  */
 class Solution {
     public int minDepth(TreeNode root) {
-        int depth = 0;
         if (root == null) return 0;
+        return dfs(root, 1);
+    }
 
-        Queue<TreeNode> q = new LinkedList<>();
-        q.offer(root);
+    public int dfs(TreeNode root, int depth) {
+        if (root.left == null && root.right == null)
+            return depth;
+        
+        int depth1 = Integer.MAX_VALUE, depth2 = Integer.MAX_VALUE;
+        if (root.left != null)
+            depth1 = dfs(root.left, depth + 1);
+        if (root.right != null)
+            depth2 = dfs(root.right, depth + 1);
 
-        while(!q.isEmpty()) {
-            int n = q.size();
-            depth++;
-            for (int i = 0; i < n; i++) {
-                TreeNode node=q.poll();
-
-                if (node.left != null)
-                    q.offer(node.left);
-                if (node.right != null)
-                    q.offer(node.right);
-                if (node.left == null && node.right == null)
-                    return depth;
-            }
-        }
-
-        return depth;
+        return Math.min(depth1, depth2);
     }
 }

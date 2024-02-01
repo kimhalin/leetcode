cf01ad0f352f735830c29134dd79693f7c336218
HaLin Kim <75435113+kimhalin@users.noreply.github.com>
2024-02-01 16:48:08 +0900

    Time: 0 ms (100%), Space: 42.8 MB (92.36%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -13,12 +13,11 @@
  *     }
  * }
  */
-public class Solution {
+
+class Solution {
     public boolean hasPathSum(TreeNode root, int sum) {
-        if(root == null) return false;
-    
-        if(root.left == null && root.right == null && sum - root.val == 0) return true;
-    
+        if (root == null) return false;
+        if (root.left == null && root.right == null && sum - root.val == 0) return true;
         return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
     }
 }
